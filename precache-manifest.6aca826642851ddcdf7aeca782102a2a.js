0ab2a8ec124084a118c12ae1baf9138716e96257
Mitch <mitch@artprocessors.net>
2019-02-11 15:58:56 +1100

    Updates

---
+++ precache-manifest.6aca826642851ddcdf7aeca782102a2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c456262e30d547adee61",
+    "url": "/static/js/main.c456262e.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "c456262e30d547adee61",
+    "url": "/static/css/main.2778210a.chunk.css"
+  },
+  {
+    "revision": "0002b54c71f72f0954783d6007e2d01d",
+    "url": "/index.html"
+  }
+];
